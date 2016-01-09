1b9e123802ed3719b0893e3306bea7584f6e9ca2
Donatas Abraitis <donatas@vinted.com>
2016-01-10 00:30:35 +0200

    A little bit refactoring

--- razor.cr
+++ razor.cr
@@ -2,89 +2,77 @@ require "redis"
 
 class Razor
 
-  def initialize(ttl, soa, banner, host, port)
+  def initialize(host = "127.0.0.1", port = 6379, types = %w(A), ttl = 30, banner = "Razor DNS backend", soa = "hostinger.com. hostmaster.hostinger.com. 2015123006 600 600 604800 600")
+    @types = types
+    @banner = banner
     @ttl = ttl
     @soa = soa
-    @banner = banner
     @redis = Redis.new(host, port)
   end
 
-  def ttl
-    @ttl
+  def run!
+    banner
+    mainLoop
   end
 
-  def soa
-    @soa
+  def mainLoop
+    loop do
+      name, qtype = parseQuery STDIN.read_line
+
+      case qtype
+      when "SOA"
+        options = {
+          :name => name,
+          :type => qtype,
+          :content => @soa
+        }
+        answer options
+      when "ANY"
+        @types.each do |type|
+          content = getDataRedis(type, name)
+          next unless content
+          options = {
+            :name => name,
+            :type => type,
+            :content => content
+          }
+          answer options
+        end
+      end
+      finish
+    end
   end
 
-  def getDataRedis(qtype, name)
+  private def getDataRedis(qtype, name)
     @redis.srandmember("#{name}-#{qtype}")
   end
 
-  def answer(options = {} of String => String|Int32)
+  private def answer(options = {} of Symbol => String|Int32)
     options = {
-      "id" => -1,
-      "class" => "IN",
-      "ttl" => @ttl
+      :id => -1,
+      :class => "IN",
+      :ttl => @ttl
     }.merge options
-    respond "DATA", options["name"], options["class"], options["type"], options["ttl"], options["id"], options["content"]
+    respond "DATA", options[:name], options[:class], options[:type], options[:ttl], options[:id], options[:content]
   end
 
-  def respond(*args)
+  private def respond(*args)
     STDOUT.print(args.join("\t") + "\n")
   end
 
-  def finish
+  private def finish
     respond "END"
   end
 
-  def banner
+  private def banner
     STDIN.read_line
     respond "OK", @banner
   end
 
-  def parseQuery(input)
-    query = input.chomp.split("\t")
-    if query.size >= 6
-      name = query[1]
-      qtype = query[3]
-    end
+  private def parseQuery(input)
+    _, name, _, qtype, _, _ = input.chomp.split("\t")
     return name, qtype
   end
 end
 
-dns = Razor.new(30,
-                "example.com. hostmaster.example.com. 2015123006 600 600 604800 600",
-                "Razor DNS backend",
-                "127.0.0.1",
-                6379)
-
-dns.banner
-
-while true
-
-  name, qtype = dns.parseQuery STDIN.read_line
-
-  case qtype
-  when "SOA"
-    options = {
-      "name" => name,
-      "type" => qtype,
-      "content" => dns.soa
-    }
-    dns.answer options
-  when "ANY"
-    %w(A AAAA).each do |type|
-      content = dns.getDataRedis(type, name)
-      next unless content
-      options = {
-        "name" => name,
-        "type" => type,
-        "content" => content
-      }
-      dns.answer options
-    end
-  end
-
-  dns.finish
-end
+Razor.new.run!
